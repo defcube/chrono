f52a3d7843c19c3e059887d3ac9f597a87990615
pgatt <gattster@gmail.com>
2015-08-22 16:28:42 -0700

    Playing with github presentation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # chrono
 Go library for passing time, such as waiting for a callback function to return true
+
+See godoc.html for usage details
---
+++ waitfor.go
@@ -0,0 +1,52 @@
+package chrono
+
+import (
+	"errors"
+	"time"
+)
+
+type WaitForSettings struct {
+
+	// Wait until this returns true, or MaxWaitTime expires
+	Test func() bool
+	MaxWaitTime time.Duration
+
+	// How long to sleep between retries of the test func?
+	SleepTime time.Duration
+
+}
+
+// A constructor for `WaitForSettings`
+func MakeWaitForSettings(t func() bool) *WaitForSettings {
+	return &WaitForSettings{
+		Test:           t,
+		SleepTime:   30 * time.Millisecond,
+		MaxWaitTime: 10 * time.Second,
+	}
+}
+
+// Convenience function that waits for the default settings
+func WaitFor(w func() bool) error {
+	return MakeWaitForSettings(w).Wait()
+}
+
+func MustWaitFor(w func() bool) {
+	err := WaitFor(w)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (w *WaitForSettings) Wait() error {
+	startTime := time.Now()
+	for {
+		time.Sleep(w.SleepTime)
+		if w.Test() {
+			break
+		}
+		if time.Since(startTime) > w.MaxWaitTime {
+			return errors.New("Waited too long, giving up")
+		}
+	}
+	return nil
+}
